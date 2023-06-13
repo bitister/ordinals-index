874147e61f428c4dccb37daf12094bf6d949f197
hayden <cfduan@outlook.com>
2023-06-13 15:45:35 +0800

    new

--- syncer/ord/worker.go
+++ syncer/ord/worker.go
@@ -105,7 +105,6 @@ func (w *Worker) parseInscriptionInfo(uid string) (map[string]interface{}, error
 			// convert "2023-05-28 03:28:17 UTC" to time.Time
 			v, _ := time.Parse("2006-01-02 15:04:05 UTC", value)
 			//v, _ := strconv.ParseUint(value, 10, 64)
-			beego.Info("timestamp====:", v.Unix())
 			details[key] = v.Unix()
 		case "genesis_height":
 			v, _ := strconv.ParseUint(value, 10, 64)
