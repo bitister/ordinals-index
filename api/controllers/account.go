5a7e342b32ad13fe8cc557374c9a1b914c92ace0
hayden <cfduan@outlook.com>
2023-06-14 14:52:14 +0800

    new

--- api/controllers/account.go
+++ api/controllers/account.go
@@ -44,6 +44,38 @@ type Domain struct {
   - @return_param data	nil	返回数据
   - @number 99
 */
+/**
+* showdoc
+* @catalog API接口/域名查询
+* @title 域名查询
+* @description 域名查询的接口
+* @method post
+* @url http://54.250.244.153:8080/domains/query
+* @header token 可选 string 设备token
+* @param name 选填 string 搜索框的输入
+* @param pageNum 选填 int 页码(默认1)
+* @param pageSize 选填 int 每页数量(默认100)
+* @param typeList 选填  string数组 后缀类型([
+  "sats",
+  "btc",
+  "txt",
+  "ord",
+  "unisat",
+  "x"
+  ])
+* @param orderType 选填  int 排序方式(0:铭文序号倒序 1：铭文需要升序 2：字母升序 3：字母降序 4：铭文余额升序 5: 铭文余额降序 6: 最短字符)
+* @param startWith 选填  string 开头
+* @param endWith 选填  string 结尾
+* @param minWidth 选填  int 字符最小长度
+* @param maxWidth 选填  int 字符最大长度
+* @param notLike 选填  string 不包含
+* @param wordsType 选填  int 字符类型(1:仅含数字 2:仅含字母 3:仅含Emoji)
+* @return {"error_code":0,"data":{"uid":"1","username":"12154545","name":"吴系挂","groupid":2,"reg_time":"1436864169","last_login_time":"0"}}
+* @return_param groupid int 用户组id
+* @return_param name string 用户昵称
+* @remark 这里是备注信息
+* @number 99
+*/
 func (c *Domain) Query() {
 	inputData := make(map[string]interface{}, 0)
 	data := c.Ctx.Input.RequestBody
@@ -63,13 +95,16 @@ func (c *Domain) Query() {
 
 	pageNum, ok := inputData["pageNum"].(int)
 	if !ok {
+		beego.Info("==pageNum==", pageNum)
 		pageNum = 1
 	}
 
-	pageSize, ok := inputData["pageSize"].(int)
+	pageSize, ok := inputData["pageSize"].(float64)
 	if !ok {
+		beego.Info("==pageSize==", pageSize)
 		pageSize = 100
 	}
+	beego.Info("pageSize:", pageSize)
 
 	//category, ok := inputData["category"].(string)
 	//if !ok {
